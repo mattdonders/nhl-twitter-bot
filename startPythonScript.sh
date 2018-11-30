fb9a1fb94a495f0eeefc368fa6482d83e397c238
Matt Donders <mdonders@gmail.com>
2018-11-30 09:53:49 -0500

    Added support for virtual environments in start script.

--- startPythonScript.sh
+++ startPythonScript.sh
@@ -21,6 +21,11 @@ touch $SCRIPTPATH/$LOGNAME
 
 # Start command using nohup & send to background
 # /usr/bin/python3 /home/twitter/python/devilstwittergoalbot/checkGameStatus.py onreboot
-nohup python3.6 $SCRIPTPATH/hockey_twitter_bot.py &
+# Source the Virtual Environment (if exists)
+if [ -d "$SCRIPTPATH/env" ]; then
+  source env/bin/activate
+fi
+
+nohup python3 $SCRIPTPATH/hockey_twitter_bot.py &
 
 exit 0
