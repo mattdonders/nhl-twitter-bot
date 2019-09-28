f3ccd991f1ed3e995211936c801f3af18608f5da
Matt Donders <mdonders@gmail.com>
2019-09-27 23:22:24 -0400

    Added docstring and neat figlet header.

--- hockeygamebot/__main__.py
+++ hockeygamebot/__main__.py
@@ -1,3 +1,22 @@
+#  _                _                                         _           _
+# | |              | |                                       | |         | |
+# | |__   ___   ___| | _____ _   _  __ _  __ _ _ __ ___   ___| |__   ___ | |_
+# | '_ \ / _ \ / __| |/ / _ \ | | |/ _` |/ _` | '_ ` _ \ / _ \ '_ \ / _ \| __|
+# | | | | (_) | (__|   <  __/ |_| | (_| | (_| | | | | | |  __/ |_) | (_) | |_
+# |_| |_|\___/ \___|_|\_\___|\__, |\__, |\__,_|_| |_| |_|\___|_.__/ \___/ \__|
+#                             __/ | __/ |
+#                            |___/ |___/
+
+
+"""
+Hockey Game Bot
+~~~~~~~~~~~~~~~~~~~~~
+
+An NHL game bot that sends important events & their details
+to social media platforms (Twitter, Slack, Discord, etc).
+
+"""
+
 import logging
 
 from hockeygamebot import app
